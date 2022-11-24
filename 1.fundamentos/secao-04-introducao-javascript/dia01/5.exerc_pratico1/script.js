f1129e2d632ea35caacef13fe08cb99dc1bd4ff9
Flávio Félix <fau_felix30@hotmail.com>
2022-11-24 20:34:31 -0300

    Feito a tarefa 7

--- 1.fundamentos/secao-04-introducao-javascript/dia01/5.exerc_pratico1/script.js
+++ 1.fundamentos/secao-04-introducao-javascript/dia01/5.exerc_pratico1/script.js
@@ -80,7 +80,7 @@ if (totalAngulosPositivos) {
 
 //Exemplo: bishop (bispo) -> diagonals (diagonais)
 
-const pecaTabuleiro = "torre";
+/* const pecaTabuleiro = "torre";
 
 switch (pecaTabuleiro.toLowerCase()) {
   case 'rei':
@@ -104,5 +104,35 @@ switch (pecaTabuleiro.toLowerCase()) {
   default:
     console.log("Peça inválida!");
     break;    
-}
+} */
 
+// Utilize if/else para escrever um código que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+/* Porcentagem >= 90 -> A
+
+Porcentagem >= 80 -> B
+
+Porcentagem >= 70 -> C
+
+Porcentagem >= 60 -> D
+
+Porcentagem >= 50 -> E
+
+Porcentagem < 50 -> F
+
+O código deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100. */
+
+const nota = 80;
+
+if (nota >= 90) {
+  console.log("A");
+} else if (nota >= 80) {
+  console.log("B");
+} else if (nota >= 70) {
+  console.log("C");
+} else if (nota >= 60) {
+  console.log("D");
+} else if (nota >= 50) {
+  console.log("E");
+} else if (nota <= 50) {
+  console.log("F");
+}
