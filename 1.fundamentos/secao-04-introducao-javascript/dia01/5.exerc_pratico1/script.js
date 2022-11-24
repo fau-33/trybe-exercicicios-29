8cd5b08ff1cc44f31f92bcd1ddce3dd0a8901377
Flávio Félix <fau_felix30@hotmail.com>
2022-11-24 19:50:39 -0300

    Feito as tarefas 3 e 4

--- 1.fundamentos/secao-04-introducao-javascript/dia01/5.exerc_pratico1/script.js
+++ 1.fundamentos/secao-04-introducao-javascript/dia01/5.exerc_pratico1/script.js
@@ -15,11 +15,38 @@ console.log(a * b);
 console.log(a % b); */
 
 // Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas constantes com os valores que serão comparados.
-const numero1 = 100;
-const numero2 = 200;
+//const numero1 = 100;
+//const numero2 = 200;
 
-if (numero1 > numero2) {
+/* if (numero1 > numero2) {
   console.log("O " + numero1 + " é o maior");
 }else {
   console.log("O " + numero2 + " é o maior");
+} */
+
+// Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três constantes com os valores que serão comparados.
+//const number1 = 10;
+//const number2 = 900;
+//const number3 = 10000;
+
+/* if (number1 > number2 && number1 > number3) {
+  console.log("O " + number1 + " é o maior");
+} else if(number2 > number1 && number2 > number3) {
+  console.log("O " + number2 + " é o maior");
+} else if(number3 > number1 && number3 > number2) {
+  console.log("O " + number3 + " é o maior");
+} */
+
+// Utilize if/else para escrever um código que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+
+/* const number = 22;
+
+if (number > 0) {
+  console.log("Positivo");
+} else if (number < 0) {
+  console.log("Negativo");
+} else {
+  console.log("Zero");
 }
+ */
+
