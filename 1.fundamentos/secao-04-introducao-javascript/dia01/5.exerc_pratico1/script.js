1c9bfeef24bbc3f6ab232fa639379ca5468a20b0
Flávio Félix <fau_felix30@hotmail.com>
2022-11-24 19:36:16 -0300

    Feito o exercicio prático 1 e 2

---
+++ 1.fundamentos/secao-04-introducao-javascript/dia01/5.exerc_pratico1/script.js
@@ -0,0 +1,25 @@
+// Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+/* Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b) */
+
+//const a = 1000;
+//const b = 100;
+
+/* console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a * b);
+console.log(a % b); */
+
+// Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas constantes com os valores que serão comparados.
+const numero1 = 100;
+const numero2 = 200;
+
+if (numero1 > numero2) {
+  console.log("O " + numero1 + " é o maior");
+}else {
+  console.log("O " + numero2 + " é o maior");
+}
