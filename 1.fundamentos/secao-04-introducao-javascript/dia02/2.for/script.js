261c7056c3fb4dc671b019c4a038cb34255ae227
Flávio Félix <fau_felix30@hotmail.com>
2022-11-25 20:34:51 -0300

    Feito o exemplo e exercicio sobre for

---
+++ 1.fundamentos/secao-04-introducao-javascript/dia02/2.for/script.js
@@ -0,0 +1,18 @@
+// For
+
+let cars = ['Saab', 'Volvo', 'BMW'];
+//console.log(cars[0]); // Saab
+//console.log(cars[1]); // Volvo
+//console.log(cars[2]); // BMW
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+}
+
+// Para fixar
+// Utilize o for para imprimir os elementos da lista groceryList com o console.log():
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+  console.log(groceryList[index]);
+}
