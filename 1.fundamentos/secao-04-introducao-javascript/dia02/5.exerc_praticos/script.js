e177cbb000b70277dc5b10b3321ce5e503dd67c2
Flávio Félix <fau_felix30@hotmail.com>
2022-11-25 21:30:03 -0300

    Feito a tarefa 3

--- 1.fundamentos/secao-04-introducao-javascript/dia02/5.exerc_praticos/script.js
+++ 1.fundamentos/secao-04-introducao-javascript/dia02/5.exerc_praticos/script.js
@@ -11,7 +11,19 @@ let sum = 0;
 } */
 
 // 2.Some todos os valores contidos no array e imprima o resultado;
+/* for (let index = 0; index < numbers.length; index += 1) {
+  sum += numbers[index];
+}
+ *///console.log(sum);
+
+// 3.Calcule e imprima a média aritmética dos valores contidos no array;
+
+//A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+
 for (let index = 0; index < numbers.length; index += 1) {
   sum += numbers[index];
 }
-console.log(sum);
+
+let media = sum / numbers.length;
+console.log(media);
+
