963e6f759b82f4286f8dd5a454736e40a4d7f434
Flávio Félix <fau_felix30@hotmail.com>
2022-11-30 20:53:45 -0300

    Feito o ultimo exercício sobre numeros primos

--- 1.fundamentos/secao-04-introducao-javascript/dia03/2.exerc_pratico/script.js
+++ 1.fundamentos/secao-04-introducao-javascript/dia03/2.exerc_pratico/script.js
@@ -21,3 +21,42 @@ for (let index = 0; index < word.length; index += 1) {
 
 //console.log(reserveWord);
 
+// 3.Considere o array de strings abaixo:
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
+
+// 3.1- Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra.
+
+for (let index = 0; index < array.length; index += 1) {  
+  if (array[index].length > maiorPalavra.length) {
+    maiorPalavra = array[index];
+  }
+  if (array[index].length < menorPalavra.length) {
+    menorPalavra = array[index];
+  }
+}
+
+//console.log("A maior palavra " + maiorPalavra);
+//console.log("A menor palavra " + menorPalavra);
+
+// Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que imprima no console o maior número primo entre 2 e 50.
+
+let maiorNumeroPrimo = 0;
+
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+
+  if (isPrime) {
+    maiorNumeroPrimo = currentNumber;
+  }
+}
+
+console.log(maiorNumeroPrimo);
