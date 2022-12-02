c8975c84eda99989943df9cb7073810d057d820e
Flávio Félix <fau_felix30@hotmail.com>
2022-12-01 21:59:31 -0300

    Feito o exercicio de fixação 5 sobre funções

--- 1.fundamentos/secao-04-introducao-javascript/dia04/4.funcao/script.js
+++ 1.fundamentos/secao-04-introducao-javascript/dia04/4.funcao/script.js
@@ -140,3 +140,24 @@ function numeroCartesiano(num) {
 console.log(numeroCartesiano(-1));
 console.log(numeroCartesiano(0));
  */
+
+// 5.Faça um programa que receba três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
+
+function somaAngulosTriangulo(anguloA, anguloB, anguloC) {
+  let somaDosAngulosInternos = anguloA + anguloB + anguloC;
+  let somaDosAngulosPositivos = anguloA > 0 && anguloB > 0 && anguloC > 0;
+
+  if (somaDosAngulosPositivos) {
+    if (somaDosAngulosInternos === 180) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return 'Erro! ângulo inválido!'
+  }
+}
+
+//console.log(somaAngulosTriangulo(100, 50, 30));
+//console.log(somaAngulosTriangulo(-100, 50, 30));
+//console.log(somaAngulosTriangulo(100, 50, 40));
