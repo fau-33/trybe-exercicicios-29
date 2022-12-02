7887190a0e62628de2933ad4b920c285608176a4
Flávio Félix <fau_felix30@hotmail.com>
2022-12-02 20:12:24 -0300

    Feito os exercícios 3, 4 e 5 sobre objetos e for/in

--- 1.fundamentos/secao-04-introducao-javascript/dia04/5.exerc_objetos_forIn/script.js
+++ 1.fundamentos/secao-04-introducao-javascript/dia04/5.exerc_objetos_forIn/script.js
@@ -22,3 +22,52 @@ let mensagem = 'Bem-vinda, ' + info.personagem;
 
 info['recorrente'] = 'Sim';
 //console.log(info);
+
+// 3.Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+/* personagem
+origem
+nota
+recorrente */
+
+for (let key in info) {
+  //console.log(key);
+}
+
+// 4.Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
+/* Margarida
+Pato Donald
+Namorada do personagem principal nos quadrinhos do Pato Donald
+Sim */
+
+for (let value in info) {
+  //console.log(info[value]);
+}
+
+// 5.Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console:
+
+/* Margarida e Tio Patinhas
+Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+Ambos recorrentes // Atenção para essa última linha!
+ */
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+}
+
+for (let propriedade in info) {
+  if (
+    propriedade === 'recorrente' &&
+    info[propriedade] === 'Sim' &&
+    info2[propriedade] === 'Sim'
+  ) {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[propriedade] + ' e ' + info2[propriedade]);
+  }
+}
+
