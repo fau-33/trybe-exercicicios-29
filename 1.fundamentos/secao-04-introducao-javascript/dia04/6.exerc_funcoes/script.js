797cf8f66dbecf537aee2a81ace486d1848bd4a4
Flávio Félix <fau_felix30@hotmail.com>
2022-12-07 20:28:07 -0300

    Feito o exercicio 6 função

--- 1.fundamentos/secao-04-introducao-javascript/dia04/6.exerc_funcoes/script.js
+++ 1.fundamentos/secao-04-introducao-javascript/dia04/6.exerc_funcoes/script.js
@@ -126,3 +126,29 @@ function somaDeTodosOsNumeros(numero) {
 }
 
 //console.log(somaDeTodosOsNumeros(5));
+
+// 6.Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Considere que a string ending sempre será menor que a string word.
+
+//Valor de teste: 'trybe' e 'be'
+//Valor esperado no retorno da função: true
+//verificaFimPalavra('trybe', 'be');
+//Retorno esperado: true
+//verificaFimPalavra('joaofernando', 'fernan');
+//Retorno esperado: false
+
+function verificaFimPalavra(palavra, fimPalavra) {
+  palavra = palavra.split('');
+  fimPalavra = fimPalavra.split('');
+
+  let controle = true;
+
+  for(let index = 0; index < fimPalavra.length; index += 1) {
+    if (palavra[palavra.length - fimPalavra.length + index] != fimPalavra[index]) {
+      controle = false;
+    }
+  }
+  return controle;
+}
+
+console.log(verificaFimPalavra('trybe', 'be'));
+console.log(verificaFimPalavra('joaofernando', 'fernan')); 
