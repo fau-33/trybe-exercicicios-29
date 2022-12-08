83b33d345bfae7e912497b2f524c689b2b6ac244
Flávio Félix <fau_felix30@hotmail.com>
2022-12-07 22:39:53 -0300

    Estudado as variaveis var, let e constjavascript es6

--- 1.fundamentos/secao-04-introducao-javascript/dia05/1.variaveis/script.js
+++ 1.fundamentos/secao-04-introducao-javascript/dia05/1.variaveis/script.js
@@ -1,13 +1,13 @@
 // var, let e const
 
-function userInfo() {
+/* function userInfo() {
   let userEmail = 'maria@email.com';
 
   // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
   //console.log(userEmail);
 }
 userInfo();
-
+ */
 if (true) {
   // inicio do escopo do if
   let userAge = '20';
@@ -38,9 +38,31 @@ favoriteTechnology = 'Facial recognition';
 
 //console.log(userInfo); // { name: 'João', id: '5489-2', email: 'claudio@email.com' }
 
-const technologies = ['Javascript', 'CSS', 'HTML'];
-technologies.push('React');
-console.log(technologies); // [ 'Javascript', 'CSS', 'HTML', 'React' ]
+//const technologies = ['Javascript', 'CSS', 'HTML'];
+//technologies.push('React');
+//console.log(technologies); // [ 'Javascript', 'CSS', 'HTML', 'React' ]
 
-technologies = ['Javascript', 'CSS', 'HTML', 'React']
+//technologies = ['Javascript', 'CSS', 'HTML', 'React']
 //console.log(technologies); // Erro
+
+// Hoisting
+
+// Hoisting é um comportamento padrão do JavaScript que acontece com variáveis declaradas com var. Esse comportamento move todas as declarações dessas variáveis para o topo do escopo global, se forem declaradas fora de uma função, ou para o topo do escopo local, se declaradas dentro de uma função, independentemente de em qual linha do código a declaração foi feita.
+
+//age = 20;
+
+//var age;
+
+//console.log(age); // 20
+
+//var age;
+
+//age = 20;
+
+//console.log(age); // 20
+
+var age;
+
+console.log(age); // undefined
+
+age = 20;
