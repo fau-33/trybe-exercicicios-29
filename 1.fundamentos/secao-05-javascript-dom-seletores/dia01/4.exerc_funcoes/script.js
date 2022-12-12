826dc29835a254323621c31f42c8ea15bdd6f2e3
Flávio Félix <fau_felix30@hotmail.com>
2022-12-12 19:59:18 -0300

    Feito o exercicios 3, 4, 5 e 6

--- 1.fundamentos/secao-05-javascript-dom-seletores/dia01/4.exerc_funcoes/script.js
+++ 1.fundamentos/secao-05-javascript-dom-seletores/dia01/4.exerc_funcoes/script.js
@@ -15,3 +15,42 @@ const changeSquareToGreen = () => {
 }
 
 changeSquareToGreen();
+
+// 3.Crie e execute uma função que mude a cor do quadrado vermelho para branco;
+
+const changeSquareToWhite = () => {
+  let squareRed = document.getElementsByClassName('center-content')[0];
+  squareRed.style.backgroundColor = 'white';
+}
+
+changeSquareToWhite();
+
+// 4.Crie e execute uma função que corrija o texto da tag <h1>;
+
+const correctTitle = () => {
+  let title = document.getElementsByTagName('h1')[0];
+  title.innerHTML = "Desafio - Javascript";
+}
+
+correctTitle();
+
+// 5.Crie e execute uma função que modifique o texto da primeira tag <p> para maiúsculo;
+
+const paragraphUppercase = () => {
+  let paragraph = document.getElementsByTagName('p')[0];
+  paragraph.innerHTML = paragraph.innerHTML.toUpperCase();
+}
+
+paragraphUppercase();
+
+// 6.Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
+
+const showParagraph = () => {
+  let allParagraph = document.getElementsByTagName('p');
+
+  for (let index = 0; index < allParagraph.length; index += 1) {
+    console.log(allParagraph[index].innerHTML);
+  }
+}
+
+showParagraph();
