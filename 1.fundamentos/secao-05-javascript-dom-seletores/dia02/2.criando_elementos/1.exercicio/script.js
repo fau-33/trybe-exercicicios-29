5091e36a0addd55ac79f1b693aa2446ffd1ffe23
Flávio Félix <fau_felix30@hotmail.com>
2022-12-12 22:21:14 -0300

    Feito os exercicios de fixação sobre criação de elementos

--- 1.fundamentos/secao-05-javascript-dom-seletores/dia02/2.criando_elementos/1.exercicio/script.js
+++ 1.fundamentos/secao-05-javascript-dom-seletores/dia02/2.criando_elementos/1.exercicio/script.js
@@ -12,3 +12,16 @@ const filhoElementoOndeVoceEsta = document.createElement('section');
 filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
 elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
 
+// 3.Crie um filho para primeiroFilhoDoFilho.
+
+const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+const filhoPrimeiroFilhoDoFilho = document.createElement('section');
+filhoPrimeiroFilhoDoFilho.id = 'filhoPrimeiroFilhoDoFilho';
+primeiroFilhoDoFilho.appendChild(filhoPrimeiroFilhoDoFilho);
+
+// 4.A partir desse filho criado, acesse terceiroFilho.
+
+const terceiroFilho = filhoPrimeiroFilhoDoFilho
+.parentElement // primeiroFilhoDoFilho
+.parentElement // elementoOndeVoceEsta
+.nextElementSibling // terceiroFilho
