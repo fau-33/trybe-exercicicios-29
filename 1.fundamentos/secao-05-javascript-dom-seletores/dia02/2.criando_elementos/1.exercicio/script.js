6373f43d9b767c293abb8492201cd9391e55d755
Flávio Félix <fau_felix30@hotmail.com>
2022-12-12 22:10:19 -0300

    Feito os exercícios 1 e 2

---
+++ 1.fundamentos/secao-05-javascript-dom-seletores/dia02/2.criando_elementos/1.exercicio/script.js
@@ -0,0 +1,14 @@
+// 1.Crie um irmão para elementoOndeVoceEsta.
+
+const pai = document.getElementById('pai');
+const irmaoElementoOndeVoceEsta = document.createElement('section');
+irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+pai.appendChild(irmaoElementoOndeVoceEsta);
+
+// 2.Crie um filho para elementoOndeVoceEsta.
+
+const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+const filhoElementoOndeVoceEsta = document.createElement('section');
+filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
+elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+
