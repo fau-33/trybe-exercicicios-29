4c6dbda48595bbc37edb718da9a9dd3e71aa1b38
Flávio Félix <fau_felix30@hotmail.com>
2022-12-13 15:13:28 -0300

    Feito 4, 5 e 6 requisitos

--- 1.fundamentos/secao-05-javascript-dom-seletores/dia02/4.exerc_pratico/script.js
+++ 1.fundamentos/secao-05-javascript-dom-seletores/dia02/4.exerc_pratico/script.js
@@ -17,3 +17,21 @@ document.body.appendChild(elementMain);
 const elementSectionCenter = document.createElement('section');
 elementSectionCenter.className = 'center-content';
 elementMain.appendChild(elementSectionCenter);
+
+// 4.Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+
+const paragraph = document.createElement('p');
+paragraph.innerHTML = 'Hello World!!!';
+elementSectionCenter.appendChild(paragraph);
+
+// 5.Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+
+const elementSectionLeft = document.createElement('section');
+elementSectionLeft.className = 'left-content';
+elementMain.appendChild(elementSectionLeft);
+
+// 6.Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+
+const elementSectionRight = document.createElement('section');
+elementSectionRight.className = 'right-content';
+elementMain.appendChild(elementSectionRight);
