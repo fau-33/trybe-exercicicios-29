2f6c619b30a0d5c9ce17a44e2e655b3af4f751e0
Flávio Félix <fau_felix30@hotmail.com>
2022-12-16 20:12:11 -0300

    Feito a parte 8

--- 1.fundamentos/secao-05-javascript-dom-seletores/dia03/1.addEventListener/2.exerc_pratico/script.js
+++ 1.fundamentos/secao-05-javascript-dom-seletores/dia03/1.addEventListener/2.exerc_pratico/script.js
@@ -176,3 +176,20 @@ const newTaskSpan = (task) => {
 }
 
 newTaskSpan('projeto');
+
+// Parte 8
+/* Implemente uma função que adicione uma legenda com cor para a tarefa;
+Essa função deverá receber como parâmetro uma string (‘cor’) e criar dinamicamente um elemento de tag <div> com a classe task;
+O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada;
+O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks". */
+
+const newTaskDiv = (color) => {
+  let tasksContainer = document.querySelector('.my-tasks');
+  let newTask = document.createElement('div'); // criando uma div
+
+  newTask.className = 'task'; // adicionando className task à newTask
+  newTask.style.backgroundColor = color; // adicionando a cor recebida por pârametro com backgroundColor
+  tasksContainer.appendChild(newTask); // adiciona newTask como filho de tasksContainer
+}
+
+newTaskDiv('green');
