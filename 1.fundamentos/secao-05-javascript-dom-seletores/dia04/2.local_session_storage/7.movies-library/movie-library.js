2f896f05788e169c7d9e39d112d249caaa0ca3b6
Flávio Félix <fau_felix30@hotmail.com>
2022-12-20 14:36:16 -0300

    Finalizando o projeto extra sobre local storage e DOM

--- 1.fundamentos/secao-05-javascript-dom-seletores/dia04/2.local_session_storage/7.movies-library/movie-library.js
+++ 1.fundamentos/secao-05-javascript-dom-seletores/dia04/2.local_session_storage/7.movies-library/movie-library.js
@@ -1,11 +1,37 @@
 const movieLibrary = document.querySelector('#movie-library');
 
-const loadMovies = () => {
-  const movies = JSON.parse(localStorage.getItem('movies')) || [];
-  console.log(movies);
+const createMovieElement = (movieObj) => {
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+  const h1 = document.createElement('h1');
+  const h2 = document.createElement('h2');
+
+  img.src = movieObj.cover;
+  h1.textContent = movieObj.title;
+
+  if (movieObj.watched) {
+    h2.textContent = 'Assistido';
+  } else {
+    h2.textContent = 'Não Assistido';
+  }
+
+  div.appendChild(img);
+  div.appendChild(h1);
+  div.appendChild(h2);
+
+  return div;
 }
 
-const onload = () => {
+const loadMovies = () => {
+  const movies = JSON.parse(localStorage.getItem('movies')) || [];
+
+  for (let index = 0; index < movies.length; index += 1) {
+    const movieElement = createMovieElement(movies[index]);
+    movieLibrary.appendChild(movieElement);
+  }
+}
+
+const onLoad = () => {
   loadMovies();
 }
 
