4f755c83207b6a9d7d976724827b131cf047f15f
Flávio Félix <fau_felix30@hotmail.com>
2023-01-19 16:38:43 -0300

    Feito o exercício bonus

--- 1.fundamentos/secao-07-fluxo-de-excecao-ES6-teste-unitarios/dia01/7.exercicios_praticos/3.exerc_pratico/script.js
+++ 1.fundamentos/secao-07-fluxo-de-excecao-ES6-teste-unitarios/dia01/7.exercicios_praticos/3.exerc_pratico/script.js
@@ -84,3 +84,42 @@ const verifyPair = (obj, key, value) => {
 };
 
 //console.log(verifyPair(lesson2, 'professor', 'carlos'));
+
+//bonus:Utilizando o objeto (allLesson), crie uma função para contar quantos estudantes assistiram às aulas de Matemática.
+
+const getNumberOfStudentsMath = (obj) => {
+  let total = 0;
+  const keys = Object.keys(obj);
+
+  for (index in keys) {
+    if (obj[keys[index]].materia === 'Matematica') {
+      total += obj[keys[index]].numeroEstudantes;
+    }
+  }
+  return total;
+};
+
+//console.log(getNumberOfStudentsMath(allLessons));
+
+//Utilizando o objeto (allLesson), crie uma função que deva retornar um objeto que represente o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes.
+
+const getInfo = (obj, name) => {
+  const allLessons = [];
+  let allStudent = 0;
+  const values = Object.values(obj);
+  for (index in values) {
+    if (values[index].professor === name) {
+      allLessons.push(values[index].materia)
+      allStudent += values[index].numeroEstudantes;
+    }
+  }
+  return { aulas: allLessons, estudantes: allStudent };
+}
+
+const createReport = (allLessons, name) => {
+  const report = {};
+  report.professor = name;
+  Object.assign(report, getInfo(allLessons, name));
+  return report;
+}
+//console.log(createReport(allLessons, 'Maria Clara'));
