52974659b94a4969f009df220ce7bf5f735cdaf4
Flávio Félix <fau_felix30@hotmail.com>
2023-01-24 15:06:08 -0300

    Feito o exemplo 1

--- 1.fundamentos/secao-07-fluxo-de-excecao-ES6-teste-unitarios/dia03/1.matchers/1.exemplo/addNewSkill.js
+++ 1.fundamentos/secao-07-fluxo-de-excecao-ES6-teste-unitarios/dia03/1.matchers/1.exemplo/addNewSkill.js
@@ -4,9 +4,4 @@ const addNewSkill = (newSkill) => {
   skills.push(newSkill);
 }
 
-test('Adiciona Jest na lista de habilidades', () => {  
-  const previousLength = skills.length;
-  addNewSkill('Jest');
-  expect(skills).toContain('Jest');
-  expect(skills).toHaveLength(previousLength + 1);
-});
+module.exports = addNewSkill;
