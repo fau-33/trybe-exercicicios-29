0156a354472fff109ddefaa25d522f663a16db5f
Flávio Félix <fau_felix30@hotmail.com>
2023-01-26 16:05:27 -0300

    Feito os exemplos parte 1

---
+++ 1.fundamentos/secao-08-introducao-higher-order-functions/dia02/2.array.map/1.exemplo/script.js
@@ -0,0 +1,62 @@
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+/* const fullNames = [];
+
+for (let index = 0; index < persons.length; index += 1) {
+  fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+}
+ */
+
+const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+//console.log(fullNames);
+
+/* const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); */ // [ 1, 2, 3, 4, -5 ]
+
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = [];
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > 0) {
+    negativeNumbers.push(numbers[index] * -1);
+  } else {
+    negativeNumbers.push(numbers[index]);
+  }
+}
+
+//console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+//console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+)); 
+
+const listProducts = updateProducts(products, prices);
+//console.log(listProducts);
+
+// Diferença entre map e forEach
+
+const numeros = [1, 2, 3, 4, 5, 6];
+console.log(numeros.map((numero) => numero * 2)); // Retorno: [2, 4, 6, 8, 10, 12]
+
+const paresMenoresQueCinco = [];
+numeros.forEach((numero) => {
+  if (numero < 5 && numero % 2 === 0) {
+    paresMenoresQueCinco.push(numero);
+  }
+});
+//console.log(paresMenoresQueCinco); // Retorno: [2, 4]
+
+
