d43be4a80c36e98f21ad1ef530c4b2fbe01db5d2
Flávio Félix <fau_felix30@hotmail.com>
2023-01-31 14:17:02 -0300

    Começando o array destruturing

---
+++ 1.fundamentos/secao-08-introducao-higher-order-functions/dia05/1.array-destructuring/1.exemplos/script.js
@@ -0,0 +1,12 @@
+// Array Destructuring
+
+const user = {
+  name: 'Marlete',
+  age: 60
+};
+
+// desestruturamos a propriedade name e atribuímos como variável
+const { name } = user;
+
+// conseguimos utilizar a propriedade como variável
+console.log(name); // Marlete
