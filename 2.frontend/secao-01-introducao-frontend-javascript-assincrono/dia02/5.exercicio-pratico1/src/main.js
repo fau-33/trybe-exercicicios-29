8fd03e56d7c17d05550ff11353db8c673393410a
Flávio Félix <fau_felix30@hotmail.com>
2023-02-12 09:35:33 -0300

    Estruturando o projeto super herois

---
+++ 2.frontend/secao-01-introducao-frontend-javascript-assincrono/dia02/5.exercicio-pratico1/src/main.js
@@ -0,0 +1,8 @@
+// Importando Swall da Biblioteca sweetalert2
+import Swal from 'sweetalert2';
+
+// Criamos as nossas constantes da nossa imagem, do nome e botão
+const img = document.querySelector('#image');
+const name = document.querySelector('#name');
+const button = document.querySelector('#button');
+
