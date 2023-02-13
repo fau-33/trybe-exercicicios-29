6e9c228bfb911b581d0b2491ca21fc3fcff72171
Flávio Félix <fau_felix30@hotmail.com>
2023-02-13 16:12:22 -0300

    Código com as funções criadas

--- 2.frontend/secao-01-introducao-frontend-javascript-assincrono/dia03/casa-de-cambio/src/main.js
+++ 2.frontend/secao-01-introducao-frontend-javascript-assincrono/dia03/casa-de-cambio/src/main.js
@@ -1,1 +1,9 @@
+import { renderCoins } from './components';
 import './style.css';
+
+const fakeCoins = [
+  { name: 'USD', value: 5.280 },
+  { name: 'EUR', value: 5.570 },
+];
+
+renderCoins(fakeCoins, 'BRL');
