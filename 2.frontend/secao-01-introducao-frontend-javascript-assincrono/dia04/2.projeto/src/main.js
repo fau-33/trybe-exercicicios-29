0bce52a443ec71913fd8dd1c87a3c02a0b93227f
Flávio Félix <fau_felix30@hotmail.com>
2023-02-14 17:41:06 -0300

    Feito o projeto CEP

---
+++ 2.frontend/secao-01-introducao-frontend-javascript-assincrono/dia04/2.projeto/src/main.js
@@ -0,0 +1,20 @@
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const preEl = document.querySelector('pre');
+import Swal from 'sweetalert2';
+
+buttonEl.addEventListener('click', handleClick);
+
+async function handleClick() {
+  const cep = inputEl.value;
+
+  try {
+    const result = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await result.json();
+    console.log(data);
+    preEl.innerHTML = JSON.stringify(data);
+    return data;
+  } catch (error) {
+    Swal.fire('Ops', error.message, 'error');
+  }
+}
