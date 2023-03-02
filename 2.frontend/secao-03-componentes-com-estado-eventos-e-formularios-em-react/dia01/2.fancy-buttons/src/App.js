3ba97a063ae7b80817cb3b95a864847ca7ab6ecd
Flávio Félix <fau_felix30@hotmail.com>
2023-03-02 19:04:34 -0300

    Feito os estados dos botões

--- 2.frontend/secao-03-componentes-com-estado-eventos-e-formularios-em-react/dia01/2.fancy-buttons/src/App.js
+++ 2.frontend/secao-03-componentes-com-estado-eventos-e-formularios-em-react/dia01/2.fancy-buttons/src/App.js
@@ -8,27 +8,48 @@ class App extends React.Component {
     this.handleButtonTwo = this.handleButtonTwo.bind(this)
     this.handleButtonThree = this.handleButtonThree.bind(this)
 
+    // Estado Inicial
+    this.state = {
+      clicksBtnOne: 0,
+      clicksBtnTwo: 0,
+      clicksBtnThree: 0,
+    }
+
   }
+  // Estado 2 nas funções
   handleButtonOne() {
-    console.log(this);
     console.log('Clicou no botão 1!!');
+    this.setState((prevState) => ({
+      clicksBtnOne: prevState.clicksBtnOne + 1,
+    }))
   }
 
   handleButtonTwo() {
-    console.log(this);
     console.log('Clicou no botão 2!!');
+    this.setState((prevState) => ({
+      clicksBtnTwo: prevState.clicksBtnTwo + 1,
+    }))
   }
 
   handleButtonThree() {
-    console.log(this);
     console.log('Clicou no botão 3!!');
+    this.setState((prevState) => ({
+      clicksBtnThree: prevState.clicksBtnThree + 1,
+    }))
   }
   render() {
+    const { clicksBtnOne, clicksBtnTwo, clicksBtnThree } = this.state;
     return(
       <div>
-        <button type="button" onClick={this.handleButtonOne}>Botão 1</button>
-        <button type="button" onClick={this.handleButtonTwo}>Botão 2</button>
-        <button type="button" onClick={this.handleButtonThree}>Botão 3</button>
+        <button type="button" onClick={this.handleButtonOne}>
+          {`Cliques botão 1: ${clicksBtnOne}`}
+        </button>
+        <button type="button" onClick={this.handleButtonTwo}>
+          {`Cliques botão 2: ${clicksBtnTwo}`}
+        </button>
+        <button type="button" onClick={this.handleButtonThree}>
+          {`Cliques botão 3: ${clicksBtnThree}`}
+        </button>
       </div>
     );
   }
