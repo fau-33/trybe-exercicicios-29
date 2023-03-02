0ae10043422e31fdd4193ffee6476c0445e46777
Flávio Félix <fau_felix30@hotmail.com>
2023-03-02 15:59:45 -0300

    Acrescentando a classe constructor nos eventos nas funções

--- 2.frontend/secao-03-componentes-com-estado-eventos-e-formularios-em-react/dia01/2.fancy-buttons/src/App.js
+++ 2.frontend/secao-03-componentes-com-estado-eventos-e-formularios-em-react/dia01/2.fancy-buttons/src/App.js
@@ -1,27 +1,34 @@
 import React from "react";
 
-function handleButtonOne() {
-  console.log('Clicou no botão 1!!');
-}
-handleButtonOne();
-
-function handleButtonTwo() {
-  console.log('Clicou no botão 2!!');
-}
-handleButtonTwo();
-
-function handleButtonThree() {
-  console.log('Clicou no botão 3!!');
-}
-handleButtonThree();
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.handleButtonOne = this.handleButtonOne.bind(this)
+    this.handleButtonTwo = this.handleButtonTwo.bind(this)
+    this.handleButtonThree = this.handleButtonThree.bind(this)
+
+  }
+  handleButtonOne() {
+    console.log(this);
+    console.log('Clicou no botão 1!!');
+  }
+
+  handleButtonTwo() {
+    console.log(this);
+    console.log('Clicou no botão 2!!');
+  }
+
+  handleButtonThree() {
+    console.log(this);
+    console.log('Clicou no botão 3!!');
+  }
   render() {
     return(
       <div>
-        <button onClick={handleButtonOne}>Botão 1</button>
-        <button onClick={handleButtonTwo}>Botão 2</button>
-        <button onClick={handleButtonThree}>Botão 3</button>
+        <button type="button" onClick={this.handleButtonOne}>Botão 1</button>
+        <button type="button" onClick={this.handleButtonTwo}>Botão 2</button>
+        <button type="button" onClick={this.handleButtonThree}>Botão 3</button>
       </div>
     );
   }
