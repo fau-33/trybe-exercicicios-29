33ac81c4de0ae5dc317bc85f6d3b95ea58617733
Flávio Félix <fau_felix30@hotmail.com>
2023-03-03 16:35:32 -0300

    Feito um componente select inputs para o formulario

--- 2.frontend/secao-03-componentes-com-estado-eventos-e-formularios-em-react/dia02/my-form-1.0/src/components/Form.jsx
+++ 2.frontend/secao-03-componentes-com-estado-eventos-e-formularios-em-react/dia02/my-form-1.0/src/components/Form.jsx
@@ -1,61 +1,100 @@
 import React, { Component } from "react";
+import SelectInput from "./SelectInput";
 
 class Form extends Component {
   constructor() {
     super();
 
     this.state = {
+      name: '',
       email: '',
+      age: '',
+      anecdote: '',
+      terms: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
-    
+
   }
 
-  handleChange(event) {
+  handleChange({ target }) {
+    const { name } = target;
+    const value = (target.type === 'checkbox') ? target.checked : target.value;
+
     this.setState({
-      email: event.target.value,
+      [name]: value,
     });
   }
 
 
   render() {
-    const { email } = this.state
+    const { name, email, age, anecdote, terms } = this.state;
 
-    
+
     return (
       <div>
         <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
         <form className="name">
-          <label htmlFor="name">
-            Nome:
-            <input type="text" name="name" id="name" />
-          </label>
+          <fieldset>
+            <legend>Informações Pessoais</legend>
 
-          <label htmlFor="email">
-            Email:
-            <input
-              id="email"
-              name="email"
-              type="email"
-              onChange={ this.handleChange }
-              value={ email }
+            <label htmlFor="name">
+              Nome:
+              <input
+                type="text"
+                name="name"
+                id="name"
+                onChange={this.handleChange}
+                value={name}
+              />
+            </label>
+
+            <label htmlFor="email">
+              Email:
+              <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={this.handleChange}
+                value={email}
+              />
+            </label>
+
+            <SelectInput
+              onChange={this.handleChange}
+              age={age}
             />
-          </label>
+          </fieldset>
 
-          <label htmlFor="age">
-            Idade:
-            <select name="age" id="age" defaultValue="">
-              <option value="">Selecione</option>
-              <option value="adult">Maior que 18</option>
-              <option value="underage">Menor que 18</option>
-            </select>
-          </label>
-
+          <fieldset>
+            <legend>Texto e arquivos</legend>
           <label htmlFor="anecdote">
             Anedota:
-            <textarea name="anecdote" id="anecdote" cols="30" rows="10"></textarea>
+            <textarea
+              name="anecdote"
+              id="anecdote"
+              cols="30"
+              rows="10"
+              onChange={this.handleChange}
+              value={anecdote}
+            ></textarea>
           </label>
+
+          <input type="file" />
+
+          </fieldset>
+
+          <label htmlFor="terms">
+            <input
+              type="checkbox"
+              name="terms"
+              id="terms"
+              onChange={this.handleChange}
+              value={terms}
+            />
+            Concordo com termos e acordo
+          </label>
+
         </form>
       </div>
     );
