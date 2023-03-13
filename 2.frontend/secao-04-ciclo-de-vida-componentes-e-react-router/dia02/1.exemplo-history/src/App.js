1757f4d4892de73d529cea1bde1e70a99b0a546a
Flávio Félix <fau_felix30@hotmail.com>
2023-03-13 15:38:09 -0300

    Configuração inicial do App

--- 2.frontend/secao-04-ciclo-de-vida-componentes-e-react-router/dia02/1.exemplo-history/src/App.js
+++ 2.frontend/secao-04-ciclo-de-vida-componentes-e-react-router/dia02/1.exemplo-history/src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Welcome from './pages/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <h1>History</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route path="/welcome" component={ Welcome } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
