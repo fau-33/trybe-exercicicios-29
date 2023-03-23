54e01cdf14dce9e4115643a743fdb0d8960e3290
Flávio Félix <fau_felix30@hotmail.com>
2023-03-23 20:03:17 -0300

    Exemplo de testes RTL em um pequeno formulário

--- 2.frontend/secao-06-testes-automatizados-com-react-testing-library/dia01/1.testes-react/src/App.test.js
+++ 2.frontend/secao-06-testes-automatizados-com-react-testing-library/dia01/1.testes-react/src/App.test.js
@@ -1,8 +1,23 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Verificando se existe um campo Email', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputEmail = screen.getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveProperty('type', 'email');
+ 
+});
+test('Verificando se existe dois botões', () => {
+  render(<App />);
+  const btn = screen.getAllByRole('button');
+  expect(btn).toHaveLength(2);
+});
+test('Verificando se existe o botão de enviar', () => {
+  render(<App />);
+  const btnSend = screen.getByTestId('id-send');
+  expect(btnSend).toBeInTheDocument();
+  expect(btnSend).toHaveProperty('type', 'button');
+  expect(btnSend).toHaveValue('Enviar');
 });
