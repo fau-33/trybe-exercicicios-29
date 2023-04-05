cccdebf63f5ad4fe658ddb18fb2c93f5a51a8a57
Flávio Félix <fau_felix30@hotmail.com>
2023-04-05 15:45:04 -0300

    Feito o exercicio

--- 2.frontend/secao-07-gerenciamento-de-estado-com-redux/dia02/1.my-app/src/App.js
+++ 2.frontend/secao-07-gerenciamento-de-estado-com-redux/dia02/1.my-app/src/App.js
@@ -1,24 +1,31 @@
-import './App.css';
 import React from 'react';
 import { connect } from 'react-redux';
+import { actionCreator, clickCounter } from './redux/actions';
 
 class App extends React.Component {
-  render(){
-    const { countState } = this.props;
-    
+  render() {
+    const { dispatch, countState, clicksCount } = this.props;
+
+    const dispatchAll = (add = 1) => {
+      dispatch(actionCreator(add));
+      dispatch(clickCounter());
+    };
+
     return (
-      <div className='App'>
-      <h1>Contador</h1>
-      <p>{ countState }</p>
-      <button>Incrementar 1</button>
-      <button>Incrementar 5</button>
-    </div>      
+      <div>
+        <h1>Contador</h1>
+        <h2>{countState}</h2>
+        <button onClick={() => dispatchAll()}>Incrementa 1</button>
+        <button onClick={() => dispatchAll(5)}>Incrementa 5</button>
+        <h3>Número de clicks: {clicksCount}</h3>
+      </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
   countState: state.count,
+  clicksCount: state.clicks,
 });
 
 export default connect(mapStateToProps)(App);
--- 2.frontend/secao-07-gerenciamento-de-estado-com-redux/dia02/1.my-app/src/redux/index.js
+++ 2.frontend/secao-07-gerenciamento-de-estado-com-redux/dia02/1.my-app/src/redux/index.js
@@ -1,10 +1,13 @@
 import { legacy_createStore as createStore } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
+import counterReducer from './reducers/counterReducer';
 
-const INITIAL_STATE = { count: 0 };
 
-const reducer = (state = INITIAL_STATE, action) => state; 
+export const actionCreator = (increment = 1) => ({ 
+  type: 'INCREMENT_COUNTER',
+  payload: increment,
+});
 
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(counterReducer, composeWithDevTools());
 
 export default store;
