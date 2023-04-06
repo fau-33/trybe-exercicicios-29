1663a6e21bebbe8bcaaefa71557ce3debc797a3b
Flávio Félix <fau_felix30@hotmail.com>
2023-04-06 16:10:18 -0300

    Feito o exemplo guiado

--- 2.frontend/secao-07-gerenciamento-de-estado-com-redux/dia04/doguinhos-app/src/App.js
+++ 2.frontend/secao-07-gerenciamento-de-estado-com-redux/dia04/doguinhos-app/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import { fetchDogImage } from "./redux/actions";
+
+//import "./style.css";
+
+class App extends React.Component {
+  render() {
+    const { isFetching, imageURL, dispatch } = this.props;
+
+    if (isFetching) return <p>Carregando...</p>;
+
+    return (
+      <div>
+        <button
+          onClick={() => { dispatch(fetchDogImage()) }}
+          type="button"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Novo Doguinho
+        </button>
+        {
+          imageURL &&
+          <img
+            src={imageURL}
+            alt="Imagem de um cachorro aleatório"
+          />
+        }
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  imageURL: state.imageURL,
+  isFetching: state.isFetching,
+});
+
+export default connect(mapStateToProps)(App);
+
