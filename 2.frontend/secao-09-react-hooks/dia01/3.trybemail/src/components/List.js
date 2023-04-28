5754bc5f81163b0682728d64b51835da59002c48
Flávio Félix <fau_felix30@hotmail.com>
2023-04-28 16:08:54 -0300

    Feito o exericio pratico

--- 2.frontend/secao-09-react-hooks/dia01/3.trybemail/src/components/List.js
+++ 2.frontend/secao-09-react-hooks/dia01/3.trybemail/src/components/List.js
@@ -1,18 +1,45 @@
 import PropTypes from 'prop-types';
-/* import { READ, UNREAD } from '../contants';
-import readIcon from '../icons/read.svg';
-import unreadIcon from '../icons/unread.svg'; */
 
-function List({ messages }) {
+
+import { READ, UNREAD } from './constants';
+
+import readIcon from '../icons/read.svg';
+import unreadIcon from '../icons/unread.svg';
+
+function List({ messages, setMessageStatus }) {
   return (
     <ul className="messages-list">
-      {messages.map((message) => (
-        <li key={ message.id }>
-          <p className="message-title">
-            {message.title}
-          </p>
-        </li>
-      ))}
+      {messages.map((message) => {
+        const messageClass = message.status === READ
+          ? 'message-read'
+          : 'message-unread';
+
+        return (
+          <li key={ message.id }>
+            <p
+              className={ `message-title ${messageClass}` }
+            >
+              {message.title}
+            </p>
+            <div>
+              <button
+                type="button"
+                title="Marcar como lida"
+                onClick={ () => setMessageStatus(message.id, READ) }
+              >
+                <img src={ readIcon } alt="" />
+              </button>
+              <button
+                type="button"
+                title="Marcar como não lida"
+                onClick={ () => setMessageStatus(message.id, UNREAD) }
+              >
+                <img src={ unreadIcon } alt="" />
+              </button>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
@@ -25,6 +52,7 @@ List.propTypes = {
       status: PropTypes.number,
     }),
   ).isRequired,
+  setMessageStatus: PropTypes.func.isRequired,
 };
 
 export default List;
