6aafd00ca015534eae0b82ef93b10fd19f14ef53
Flávio Félix <fau_felix30@hotmail.com>
2023-06-14 21:05:59 -0300

    Feito o exercicio 4 e 5

--- 3.backend/secao-04-intro-web-node/dia01/my-script-5/bmi.js
+++ 3.backend/secao-04-intro-web-node/dia01/my-script-5/bmi.js
@@ -1,3 +1,5 @@
+const readline = require('readline-sync');
+
 const weightInKg = 80;
 const heightInCm = 178;
 
@@ -12,10 +14,57 @@ function handleBmi(weight, height) {
     return bmi;
 };
 
-function main() {
-    const bmi = handleBmi(weightInKg, heightInCm);
+const BMI_MAX_AND_MIN = {
+    'Underweight': {
+      minBMI: 0,
+      maxBMI: 18.4,
+    },
+    'Normal Weight': {
+      minBMI: 18.5,
+      maxBMI: 24.9,
+    },
+    'Overweight': {
+      minBMI: 25,
+      maxBMI: 29.9,
+    },
+    'Obese Class I': {
+      minBMI: 30.0,
+      maxBMI: 34.9,
+    },
+    'Obese Class II': {
+      minBMI: 35,
+      maxBMI: 39.9,
+    },
+    'Obese Class III': {
+      minBMI: 40,
+      maxBMI: 100, // Um valor default máximo qualquer, impossível de alcançar no imc.
+    },
+  };
 
-    console.log(`BMI: ${bmi.toFixed(2)}`);
+
+function handleBMIResult(bmi) {
+    const statuses = Object.keys(BMI_MAX_AND_MIN);
+
+    const resultFind = statuses.find((status) => {
+        const { maxBMI, minBMI } = BMI_MAX_AND_MIN[status];
+        
+        return bmi >= minBMI && bmi <= maxBMI;
+    });
+
+    return resultFind;
+}  
+
+function main() {
+
+    //const weight = readline.questionFloat('Qual é seu peso? (kg)');
+    //const height = readline.questionInt('Qual a sua altura? (cm)');
+
+    //const bmi = handleBmi(weight, height);
+    const bmiResult = handleBMIResult(bmi);
+
+    console.log(`bmiResult: ${bmiResult}`);
+
+    //console.log(`BMI: ${bmi.toFixed(2)}`);
 }
 
 main();
