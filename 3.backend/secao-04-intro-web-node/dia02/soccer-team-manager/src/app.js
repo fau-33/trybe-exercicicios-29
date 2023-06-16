82dc05093deef4fa4bdca1ce48a3c78a39a61e9a
Flávio Félix <fau_felix30@hotmail.com>
2023-06-15 22:58:16 -0300

    Estudado os métodos get e post

--- 3.backend/secao-04-intro-web-node/dia02/soccer-team-manager/src/app.js
+++ 3.backend/secao-04-intro-web-node/dia02/soccer-team-manager/src/app.js
@@ -2,4 +2,27 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+// eslint-disable-next-line sonarjs/no-unused-collection
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
 module.exports = app;
