e4cc269d7583b451d972f4716d5388923d28a852
Flávio Félix <fau_felix30@hotmail.com>
2023-06-17 16:46:32 -0300

    Feito o exercício pratico do dia 4.2

--- 3.backend/secao-04-intro-web-node/dia02/movies-node-2/src/app.js
+++ 3.backend/secao-04-intro-web-node/dia02/movies-node-2/src/app.js
@@ -17,6 +17,20 @@ const readFile = async () => {
     }
   };
 
+app.get('/movies/search', async (req, res) => {
+    try {
+        const { q } = req.query;
+        const movies = await readFile();
+        if (q) {
+                const filteredMovies = movies.filter((element) => element.movie.includes(q));
+            return res.status(200).json(filteredMovies);
+        }
+        res.status(200).end();
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+    }
+});  
+
 app.get('/movies/:id', async (req, res) => {
     try {
         const movies = await readFile();
@@ -48,6 +62,34 @@ app.post('/movies', async (req, res) => {
         const allMovies = JSON.stringify([...movies, ...newMovie]);
         await fs.writeFile(moviesPath, allMovies);
         res.status(201).json(newMovie);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+});
+
+app.put('/movies/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { movie, price } = req.body;
+        const movies = await readFile();
+        const index = movies.findIndex((element) => element.id === Number(id));
+        movies[index] = { id: Number(id), movie, price };
+        const updateMovies = JSON.stringify(movies, null, 2);
+        await fs.writeFile(moviesPath, updateMovies);
+        res.status(200).json(movies[index]);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+});
+
+app.delete('/movies/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movies = await readFile();
+        const filteredMovies = movies.filter((movie) => movie.id !== Number(id));
+        const updateMovies = JSON.stringify(filteredMovies, null, 2);
+        await fs.writeFile(moviesPath, updateMovies);
+        res.status(204).end();
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
