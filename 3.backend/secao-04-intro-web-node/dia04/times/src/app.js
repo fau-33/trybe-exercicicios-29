7df7b2a71a85079ee62bd4330c1461520ea0d095
Flávio Félix <fau_felix30@hotmail.com>
2023-06-19 19:52:18 -0300

    Feito o exercicio 1 e 2

---
+++ 3.backend/secao-04-intro-web-node/dia04/times/src/app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+
+const app = express();
+
+let nextId = 3;
+const teams = [
+  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
+  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
+];
+
+app.use(express.json());
+
+app.get('/teams', (req, res) => res.json(teams));
+
+const existingId = (req, res, next) => {
+  const id = Number(req.params.id);
+
+  if (teams.some((t) => t.id === id)) {
+    // se existe, a requisição segue para o próximo middleware
+    return next();
+  }
+  // se não existe, então vamos retornar o status HTTP 404
+  res.sendStatus(400);
+};
+
+// usa o middleware
+app.get('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+  res.json(team);
+});
+
+const validateTeam = (req, res, next) => {
+  const requiredProperties = ['nome', 'sigla'];
+  if (requiredProperties.every((property) => property in req.body)) {
+    next(); // Chama o próximo middleware
+  } else {
+    res.status(400); // Ou já responde avisando que deu errado
+  }
+};
+
+app.post('/teams', validateTeam, (req, res) => {
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
+});
+
+app.put('/teams/:id', existingId, validateTeam, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+
+  const index = teams.indexOf(team);
+  const updated = { id, ...req.body };
+  teams.splice(index, 1, updated);
+  res.status(201).json(updated);  
+});
+
+app.delete('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+  const index = teams.indexOf(team);
+  teams.splice(index, 1);
+  res.sendStatus(204);
+});
+
+module.exports = app;
