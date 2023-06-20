84d57ae965cd8f830b274d941a9e2d760967e278
Flávio Félix <fau_felix30@hotmail.com>
2023-06-20 16:24:44 -0300

    Feito uma rota signup e criado o generate token

--- 3.backend/secao-04-intro-web-node/dia04/ecoturismo/src/app.js
+++ 3.backend/secao-04-intro-web-node/dia04/ecoturismo/src/app.js
@@ -5,6 +5,7 @@ const validateCreatedAt = require('../middlewares/validateCreatedAt');
 const validateDescription = require('../middlewares/validateDescription');
 const validatePrice = require('../middlewares/validatePrice');
 const validateName = require('../middlewares/validateName');
+const generateToken = require('../utils/generateToken');
 
 
 
@@ -17,6 +18,17 @@ app.use(express.json());
 
 app.post('/activities', validateDifficulty, validateRating, validateCreatedAt, validateDescription, validatePrice, validateName, (_req, res) => {
     res.status(201).json({ message: 'Atividade registrada com sucesso!'});
+});
+
+app.post('/signup', (_req, res) => {
+    const { email, password, firstName, phone } = req.body;
+
+    if ([email, password, firstName, phone].includes(undefined)) {
+        return res.status(401).json({ message: 'Campos ausentes!' });
+    }
+
+    const token = generateToken();
+    return res.status(200).json({ token });
 });
 
 module.exports = app;
