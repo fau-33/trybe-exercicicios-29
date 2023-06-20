675fa4381e1747d72f9612692b756c99c715a9fc
Flávio Félix <fau_felix30@hotmail.com>
2023-06-20 16:31:36 -0300

    Feito mais um arquivo de middlewares auth

--- 3.backend/secao-04-intro-web-node/dia04/ecoturismo/src/app.js
+++ 3.backend/secao-04-intro-web-node/dia04/ecoturismo/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const auth = require('../middlewares/auth');
 const validateDifficulty = require('../middlewares/validateDifficulty');
 const validateRating = require('../middlewares/validateRating');
 const validateCreatedAt = require('../middlewares/validateCreatedAt');
@@ -10,13 +11,14 @@ const generateToken = require('../utils/generateToken');
 
 
 
+
 const app = express();
 
 module.exports = app;
 
 app.use(express.json());
 
-app.post('/activities', validateDifficulty, validateRating, validateCreatedAt, validateDescription, validatePrice, validateName, (_req, res) => {
+app.post('/activities', auth, validateDifficulty, validateRating, validateCreatedAt, validateDescription, validatePrice, validateName, (_req, res) => {
     res.status(201).json({ message: 'Atividade registrada com sucesso!'});
 });
 
