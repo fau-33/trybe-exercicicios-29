dde931f3c9fd812604ca7100893e1cebda227b6b
Flávio Félix <fau_felix30@hotmail.com>
2023-06-20 15:29:34 -0300

    Feito dos arquivos de middlewares de validação de nome e preço

--- 3.backend/secao-04-intro-web-node/dia04/ecoturismo/src/app.js
+++ 3.backend/secao-04-intro-web-node/dia04/ecoturismo/src/app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const validateName = require('../middlewares/validateName');
+const validatePrice = require('../middlewares/validatePrice');
 
 const app = express();
+
+module.exports = app;
+
+app.use(express.json());
+
+app.post('/activities', validatePrice, validateName, (_req, res) => {
+    res.status(201).json({ message: 'Atividade registrada com sucesso!'});
+});
 
 module.exports = app;
